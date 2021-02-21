28183de7d9174181aa76eeae623076cfd4eaee7d
MZH <mazahkaroma@mail.ru>
2021-02-22 00:50:14 +0300

    Отформатирован файл README.md. Выполнен весь первый блок

--- README.md
+++ README.md
@@ -5,18 +5,18 @@ ___Выполнение блоков___
 Для выполнения каждого блока задания были созданные соответствующие классы.  
 Краткая справка о процессе выполнения блоков и задач в них представленна в таблице ниже
 
-|Номер задач|Блок 1|Блок 2|Блок 3|Блок 4|Блок 5|
-|-----------|------|------|------|------|------|
-|   1       |:heavy_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|
-|   2       |:heavy_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|
-|   3       |:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|
-|   4       |:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|
-|   5       |:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|
-|   6       |:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|
-|   7       |:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|
-|   8       |:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|
-|   9       |:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|
-|   10      |:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|
+|Номер задач|Блок 1|Блок 2|Блок 3|Блок 4|Блок 5|Блок 6|
+|-----------|------|------|------|------|------|------|
+|   1       |:heavy_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|
+|   2       |:heavy_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|
+|   3       |:heavy_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|
+|   4       |:heavy_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|
+|   5       |:heavy_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|
+|   6       |:heavy_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|
+|   7       |:heavy_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|
+|   8       |:heavy_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|
+|   9       |:heavy_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|
+|   10      |:heavy_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|:white_check_mark:|
 
 ___Блок 1___
 -
--- src/PracticeTasks/Main.java
+++ src/PracticeTasks/Main.java
@@ -1,71 +1,244 @@
 package PracticeTasks;
 
-import java.util.HashMap;
-import java.util.Map;
+
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-
-        Map<String,TaskNumber> TaskMap= new HashMap<String,TaskNumber>();
-        TaskMap.put("First",TaskNumber.First);
-        TaskMap.put("Second",TaskNumber.Second);
-        TaskMap.put("Third",TaskNumber.Third);
-        TaskMap.put("Fourth",TaskNumber.Fourth);
-        TaskMap.put("Fifth",TaskNumber.Fifth);
-        TaskMap.put("Sixth",TaskNumber.Sixth);
-        TaskMap.put("Seventh",TaskNumber.Seventh);
-        TaskMap.put("Eighth",TaskNumber.Eighth);
-        TaskMap.put("Ninth",TaskNumber.Ninth);
-        TaskMap.put("Tenth",TaskNumber.Tenth);
-
-        while(true)
-        {
-            System.out.print("""
-
-                    Задания:
-                    First: Перевод минут в секунды.
-                    Second: Подсчет очков. Баскетбол.
-
-                    """);
-            System.out.println("Введите задание для выполнение: ");
-            if(sc.hasNext())
+    public static void main(String[] args) {
+            String menu = "-------------------Задание первого блока----------------------\n" +
+                            "1. convert(int minutes)\n" +
+                            "2. points(int TwoPoints, int ThreePoints)\n" +
+                            "3. footballpoints(int win, int draw, int lose)\n" +
+                            "4. divisibleByFive(int number)\n" +
+                            "5. and(boolean a,boolean b)\n" +
+                            "6. howManyWalls(int n,int width, int height)\n" +
+                            "7. squared(int b)\n" +
+                            "8. profitableGamble(double prob, double prize, double pay)\n" +
+                            "9. frames(int minutes, int frame)\n" +
+                            "10. mod(int first, int second)\n" +
+                            "0. Выход\n";
+            System.out.println(menu);
+            while(true)
             {
-                String Task = sc.nextLine();
-                switch (TaskMap.get(Task))
+                int choice;
+                try
                 {
-                    case First:
+                    choice = Integer.parseInt(getInput("Выбирте задание из списка: ")[0]);
+                }
+                catch (Exception exp)
+                {
+                    System.out.println("Ошибка: "+exp+" Повторите ввод\n");
+                    continue;
+                }
+                switch (choice)
+                {
+                    case 1:
                         while(true)
                         {
-                            TasksFromBlockOne BlockOne = new TasksFromBlockOne();
-                            System.out.print("Введите количество минут: ");
-                            if (sc.hasNextInt())
-                            {
-                                System.out.println("Количество секунд равно: " + BlockOne.convert(sc.nextInt()));
+                            try {
+                                int number = Integer.parseInt(getInput("Введите колличество минут: ")[0]);
+                                System.out.println("Колличество секунд равно: " + convert(number));
                                 break;
-                            } else System.out.println("Некорректный ввод данных. Попробуйте еще раз ");
+                            }
+                            catch (Exception exp)
+                            {
+                                System.out.println("Ошибка: "+exp+" Повторите ввод\n");
+                                continue;
+                            }
                         }
                         break;
-
-                    case Second:
+                    case 2:
                         while(true)
                         {
-                            TasksFromBlockOne BlockOne = new TasksFromBlockOne();
-                            System.out.print("Введите количество двухочковых и трехочковых через пробел: ");
-                            if (sc.hasNextInt())
-                            {
-                                System.out.println("Количество очков: " + BlockOne.points(sc.nextInt(), sc.nextInt()));
+                            try {
+                                String[] points = getInput("Введите колличество двух очковых и трех очковых: ");
+                                System.out.println("Общая сумма очков: " + points(Integer.parseInt(points[0]), Integer.parseInt(points[1])));
                                 break;
-                            } else System.out.println("Некорректный ввод данных. Попробуйте еще раз ");
+                            }
+                            catch (Exception exp)
+                            {
+                                System.out.println("Ошибка: "+exp+" Повторите ввод\n");
+                                continue;
+                            }
                         }
                         break;
-
+                    case 3:
+                        while(true)
+                        {
+                            try
+                            {
+                                String[] res = getInput("Введите результаты комманды по футболу: ");
+                                System.out.println("Общая сумма очков: " +
+                                                    footballpoints(Integer.parseInt(res[0]),
+                                                    Integer.parseInt(res[1]),
+                                                    Integer.parseInt(res[3]))
+                                                    );
+                                break;
+                            }
+                            catch (Exception exp)
+                            {
+                                System.out.println("Ошибка: "+exp+" Повторите ввод\n");
+                                continue;
+                            }
+                        }
+                        break;
+                    case 4:
+                        while(true) {
+                            try
+                            {
+                                int division = Integer.parseInt(getInput("Введите число для проверки делимости на 5: ")[0]);
+                                System.out.println("Результат проверики делимости: " + divisibleByFive(division));
+                                break;
+                            }
+                            catch (Exception exp) {
+                                System.out.println("Ошибка: " + exp + " Повторите ввод\n");
+                                continue;
+                            }
+                        }
+                        break;
+                    case 5:
+                        while (true)
+                        {
+                            try
+                            {
+                                String[] bools = getInput("Введите логические выражения: ");
+                                System.out.println("Результат проверки: " + and(Boolean.parseBoolean(bools[0]), Boolean.parseBoolean(bools[1])));
+                                break;
+                            }
+                            catch (Exception exp)
+                            {
+                                System.out.println("Ошибка: " + exp + " Повторите ввод\n");
+                                continue;
+                            }
+                        }
+                        break;
+                    case 6:
+                        while (true)
+                        {
+                            try
+                            {
+                                String[] data = getInput("Введите данные для покраски стен: ");
+                                System.out.println("Результат рассчета: " + howManyWalls(Integer.parseInt(data[0]), Integer.parseInt(data[1]), Integer.parseInt(data[2])));
+                                break;
+                            }
+                            catch (Exception exp)
+                            {
+                                System.out.println("Ошибка: " + exp + " Повторите ввод\n");
+                                continue;
+                            }
+                        }
+                        break;
+                    case 7:
+                        while (true)
+                        {
+                            try
+                            {
+                                int square = Integer.parseInt(getInput("Введите данные для рассчета квадрата числа: ")[0]);
+                                System.out.println("квадрат числа: " + squared(square));
+                                break;
+                            }
+                            catch (Exception exp)
+                            {
+                                System.out.println("Ошибка: " + exp + " Повторите ввод\n");
+                                continue;
+                            }
+                        }
+                        break;
+                    case 8:
+                        while (true)
+                        {
+                            try
+                            {
+                                String[] pribil = getInput("Введите данные для рассчета прибыли: ");
+                                System.out.println("Ответ прибыльно ли: " + profitableGamble(Double.parseDouble(pribil[0]), Double.parseDouble(pribil[1]), Double.parseDouble(pribil[2])));
+                                break;
+                            }
+                            catch (Exception exp)
+                            {
+                                System.out.println("Ошибка: " + exp + " Повторите ввод\n");
+                                continue;
+                            }
+                        }
+                        break;
+                    case 9:
+                        while (true)
+                        {
+                            try
+                            {
+                                String[] FPSdata = getInput("Введите данные для рассчета кадров: ");
+                                System.out.println("Рассчет FPS: " + frames(Integer.parseInt(FPSdata[0]), Integer.parseInt(FPSdata[1])));
+                                break;
+                            }
+                            catch (Exception exp)
+                            {
+                                System.out.println("Ошибка: " + exp + " Повторите ввод\n");
+                                continue;
+                            }
+                        }
+                        break;
+                    case 10:
+                        while (true)
+                        {
+                            try
+                            {
+                                String[] ModData = getInput("Введите данные для рассчета остатка при делении: ");
+                                System.out.println("Рассчет остатка: " + mod(Integer.parseInt(ModData[0]), Integer.parseInt(ModData[1])));
+                                break;
+                            }
+                            catch (Exception exp)
+                            {
+                                System.out.println("Ошибка: " + exp + " Повторите ввод\n");
+                                continue;
+                            }
+                        }
+                        break;
+                    case 0:
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Неверный ввод данных,повторите попытку");
+                        continue;
 
                 }
             }
-        }
     }
+
+
+
+    public static int convert(int minutes) { return  minutes*60; }
+
+    public static int points(int TwoPoints, int ThreePoints) { return TwoPoints*2+ThreePoints*3; }
+
+    public static int footballpoints(int win, int draw, int lose) { return win*2+draw*1+lose*0; }
+
+    public static boolean divisibleByFive(int number) { return number%5==0; }
+
+    public static boolean and(boolean a,boolean b) { return a && b; }
+
+    public static int howManyWalls(int n,int width, int height) { return width*height/n; }
+
+    public static int squared(int b){ return b*b; }
+
+    public static boolean profitableGamble(double prob, double prize, double pay) { return (prob*prize)-pay>0; }
+
+    public static int frames(int minutes, int frame) { return minutes*frame*60; }
+
+    public static int mod(int first, int second) { return first-second*(first/second); }
+
+    public static String[] getInput(String message)
+    {
+        String[] outputData=null;
+        System.out.println(message);
+        Scanner scanner=new Scanner(System.in);
+        if(scanner.hasNextLine())
+        {
+            outputData=scanner.nextLine().split(" ");
+        }
+
+        return outputData;
+    }
+
+
+
 }
