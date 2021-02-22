8c1991e2eed9327104399a661dc88fe7589997df
MZH <mazahkaroma@mail.ru>
2021-02-22 03:50:03 +0300

    Полность сделан блок 2, отсутствует ввод

--- src/PracticeTasks/Main.java
+++ src/PracticeTasks/Main.java
@@ -1,19 +1,24 @@
 package PracticeTasks;
 
 
+import java.util.Locale;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println(nameShuffle("A a"));
+    public static void main(String[] args)
+    {
+
 
 
     }
     public static int oppositeHouse(int rightHouse, int streetLong) { return streetLong*2-rightHouse+1; }
 
-    public static String nameShuffle(String FIO) { return FIO.split(" ")[1]+" "+FIO.split("")[0]; }
+    public static String nameShuffle(String FIO) { return FIO.split(" ")[1]+" "+FIO.split(" ")[0]; }
 
-    public static float discount(int price, int discount) { return price-(price*discount)/100; }
+    public static float discount(int price, int discount)
+    {
+        return price-(price*discount)/100;
+    }
 
     public static float differenceMaxMin(float[] mas)
     {
@@ -36,6 +41,56 @@ public class Main {
         return tmp;
     }
 
+    public static String reverse(String upcomingString)
+    {
+        String sPalindrome= "";
+        for(int i=upcomingString.length()-1;i>=0;i--)
+        {
+            sPalindrome+=upcomingString.charAt(i);
+        }
+        return sPalindrome;
 
+    }
 
+    public static float programmers(float first,float second,float third)
+    {
+        float max=first;
+        float min=first;
+        if (second < min) { min = second; }
+        if (third < min) { min = third; }
+
+        if (second > max) { max = second; }
+        if (third > max) { max = third; }
+        return max-min;
+    }
+
+    public static boolean getXO(String XO)
+    {
+        int countX=0;
+        int countY=0;
+        XO=XO.toLowerCase(Locale.ROOT);
+        for (int i=0;i<XO.length();i++)
+        {
+            if (XO.charAt(i)=='x') { countX++; }
+            if (XO.charAt(i)=='o') { countY++; }
+        }
+        return countX==countY;
+    }
+
+    public static String bomb(String Sentence)
+    {
+        if ( Sentence.contains("bomb") ) { return "DUCK!"; }
+        return "Relax, there's no bomb.";
+    }
+
+    public static boolean sameAscii(String a,String b)
+    {
+        int sumA=0;
+        int sumB=0;
+
+        for (int i=0; i<a.length(); i++) { sumA += (int)a.charAt(i); }
+        for (int i=0; i<b.length(); i++) { sumB += (int)b.charAt(i); }
+
+        return sumB==sumA;
+    }
 }
